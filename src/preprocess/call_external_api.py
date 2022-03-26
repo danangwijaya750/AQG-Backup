deda67874678de887122d57e25cb0559eaa7b3ba
Danang Wijaya <danangwijaya750@gmail.com>
2022-03-26 16:31:41 +0700

    update external api

--- src/preprocess/call_external_api.py
+++ src/preprocess/call_external_api.py
@@ -1,35 +1,71 @@
-import http.client
+# import http.client
+# import json
+
+
+# API_BASE_URL = '10.181.131.244'
+# POS_TAG_PORT = '5500'
+# NER_PORT = '10009'
+
+# def get_pos_tag(text):
+#     conn = http.client.HTTPConnection(API_BASE_URL, POS_TAG_PORT)
+#     payload = json.dumps({'text': text})
+#     headers = {
+#         "content-type": "application/json",
+#         "x-api-key": ""
+#         }
+
+#     conn.request('POST', '/', payload, headers)
+#     res = conn.getresponse()
+#     data = json.loads(res.read().decode('utf-8'))
+
+#     return data
+
+# def get_ner(text):
+#     conn = http.client.HTTPConnection(API_BASE_URL, NER_PORT)
+#     payload = json.dumps({'text': text})
+#     headers = {
+#         "content-type": "application/json",
+#         "x-api-key": ""
+#         }
+
+#     conn.request('POST', '/', payload, headers)
+#     res = conn.getresponse()
+#     data = json.loads(res.read().decode('utf-8'))
+
+#     return data
+
 import json
+import requests
+ 
+url_ner = "https://api.prosa.ai/v2/text/ner"
+url_pos = "https://api.prosa.ai/v2/text/basic-nlp"
+api_key_ner=""
+api_key_pos=""
+ 
 
-
-API_BASE_URL = '10.181.131.244'
-POS_TAG_PORT = '5500'
-NER_PORT = '10009'
 
 def get_pos_tag(text):
-    conn = http.client.HTTPConnection(API_BASE_URL, POS_TAG_PORT)
-    payload = json.dumps({'text': text})
+    data = {
+        "version": "v2",
+        "text": text,
+    }
     headers = {
-        "content-type": "application/json",
-        "x-api-key": ""
-        }
-
-    conn.request('POST', '/', payload, headers)
-    res = conn.getresponse()
-    data = json.loads(res.read().decode('utf-8'))
-
-    return data
+        "Content-Type": "application/json",
+         "x-api-key": api_key_pos
+    }
+    response = requests.post(url_pos, headers=headers, json=data)
+    jsondata=response.json()
+    return jsondata['sentences']['sentences'][0]
 
 def get_ner(text):
-    conn = http.client.HTTPConnection(API_BASE_URL, NER_PORT)
-    payload = json.dumps({'text': text})
+    data = {
+        "version": "v2",
+        "text": text,
+    }
     headers = {
-        "content-type": "application/json",
-        "x-api-key": ""
-        }
-
-    conn.request('POST', '/', payload, headers)
-    res = conn.getresponse()
-    data = json.loads(res.read().decode('utf-8'))
-
-    return data
+        "Content-Type": "application/json",
+         "x-api-key": api_key_ner
+    }
+    response = requests.post(url_ner, headers=headers, json=data)
+    jsondata=response.json()
+    return jsondata
